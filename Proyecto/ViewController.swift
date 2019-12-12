d5a2f4174d87dfcf57a8d3050df518985634258b
ramon baeza yepez <By_ramon@outlook.com>
2019-12-12 14:35:34 -0600

    Initial Commit

---
+++ Proyecto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proyecto
+//
+//  Created by ByRamon on 12/12/19.
+//  Copyright © 2019 ByRamon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
